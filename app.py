e0c40eb8fbe3546bdf426455a8f99ca785f22f38
lokesh1199 <lokesh9911@protonmail.com>
2021-11-21 00:12:54 +0530

    add admin login and panel

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import sqlite3
 
-from flask import Flask, redirect, render_template, request, send_from_directory
+from flask import Flask, redirect, render_template, request, send_from_directory, session
 
 from check import checkRoll
 from marks import getMarks, getName, parseTableName, getResultsList, getTableName
 
 app = Flask(__name__)
+# TODO Change this in production
+app.secret_key = '6d5843bbf5bdc0f249376717e6d32919715b4bd02b89'
 
 
 @app.route('/')
@@ -23,28 +25,75 @@ def roll(table):
 
 @app.route('/results')
 def results():
-    rollno = request.args.get('roll').strip()
-    tableSno = request.args.get('table').strip()
-    rollno = rollno.upper()
+    rollno = request.args.get('roll').strip().upper()
+    table = request.args.get('table').strip()
     if not checkRoll(rollno):
-        return redirect('error')
+        return redirect('/error')
 
     con = sqlite3.connect('results.db')
 
     try:
-        tableName = getTableName(tableSno, con)
+        tableName = getTableName(table, con)
         examName = parseTableName(tableName)
         marks = getMarks(con, rollno, tableName)
         name = getName(con, rollno, tableName)
 
         return render_template('results.html', marks=marks, name=name, rollno=rollno, examName=examName)
     except Exception as e:
-        return redirect('error')
+        return redirect('/error')
 
 
 @app.route('/error')
 def error():
     return render_template('error.html'), 400
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == 'admin' and password == 'admin123':
+            session['admin'] = True
+            return redirect('/admin')
+        return render_template('admin_login.html', incorrect=True)
+    else:
+        if session.get('admin'):
+            examMonths = (
+                'January',
+                'Febraury',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December'
+            )
+            examYears = [year for year in range(2020, 2030)]
+            years = ['I', 'II', 'III', 'IV']
+            sems = ['I', 'II']
+
+            return render_template('admin_panel.html', examMonths=examMonths, examYears=examYears, years=years, sems=sems)
+
+        return render_template('admin_login.html', incorrect=False)
+
+
+@app.route('/logout')
+def logout():
+    if session.get('admin'):
+        session.pop('admin', None)
+        return redirect('/admin')
+    else:
+        return redirect('/')
+
+
+@app.route('/upload')
+def upload():
+    return redirect('/error')
 
 
 # TODO Remove this in production
